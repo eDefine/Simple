e93c16629464133e5d28fa197c5124e4b3cdc4da
Patryk Syc <patryk.syc@smtsoftware.com>
2015-08-31 12:30:20 +0200

    Changed loginUser method in UserRepository

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -9,19 +9,18 @@ class UserRepository extends AbstractRepository
 {
     public function loginUser(LoginObject $loginObject)
     {
-        $queryBuilder = $this->getSelectQueryBuilder()
-            ->addWhere(sprintf('email = "%s"', $loginObject->getEmail()))
-            ->addWhere(sprintf('password = MD5(CONCAT("%s_", salt))', $loginObject->getPlainPassword()));
-
-        $query = $queryBuilder->getQuery();
-
-        $results = $query->execute();
+        $results = $this->findAll(['email' => $loginObject->getEmail()]);
 
         if (count($results) != 1) {
             throw new \RuntimeException('Cannot login');
         }
 
-        return $results[0];
+        $user = $results[0];
+        if ($user->getPassword() != md5(sprintf('%s_%s', $loginObject->getPlainPassword(), $user->getSalt()))) {
+            throw new \RuntimeException('Cannot login');
+        }
+
+        return $user;
     }
 
     public function loginUserBySessionId($sessionId)
